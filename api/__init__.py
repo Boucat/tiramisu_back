8ba74327bcb5d3a635ce102b8921cba99a9b963f
ferrannoguera <nogueravallferran@gmail.com>
2023-11-28 00:32:30 +0100

    :wrench: Created a mock user database

---
+++ api/__init__.py
@@ -0,0 +1,8 @@
+USERS = [
+    {
+        'sec_key': '318e010e48f6e209cbcb39254bb46547eb208029',
+        'id': 'ferrannoguera',
+        'name': 'Ferran Noguera',
+        'photo_url': 'https://avatars.githubusercontent.com/u/115733?v=4',
+    }
+]
