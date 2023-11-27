3aa1bca090bdd9197d7e31059b42d46cab43985a
ferrannoguera <nogueravallferran@gmail.com>
2023-11-28 00:32:48 +0100

    :sparkles: Created dependencies for user token

--- api/dependencies.py
+++ api/dependencies.py
@@ -1,6 +1,14 @@
-from fastapi import HTTPException
+import hashlib
+
+from fastapi import HTTPException, Query
+
+from api import USERS
 
 
-async def get_query_token(token: str):
-    if token != "jessica":
-        raise HTTPException(status_code=400, detail="No Jessica token provided")
+async def check_user_token(user_id: str = Query(..., title='User', description='The user parameter')):
+    hashed_token = hashlib.sha1(user_id.encode('utf-8')).hexdigest()
+    hashed_tokens_db = [token.get('sec_key') for token in USERS]
+    if hashed_token in hashed_tokens_db:
+        return user_id, hashed_token
+    else:
+        raise HTTPException(status_code=401, detail='Wrong token provided')
