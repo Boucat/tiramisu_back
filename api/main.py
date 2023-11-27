e2e277d3395433bb234444415afdd1ab0f13c55d
ferrannoguera <nogueravallferran@gmail.com>
2023-11-28 00:34:20 +0100

    :art: Improved FastAPI declaration + routings

--- api/main.py
+++ api/main.py
@@ -1,23 +1,27 @@
-from fastapi import APIRouter, FastAPI
+from api.router.tiramisu import tiramisu_router
+from api.router.review_tiramisu import review_tiramisu_router
+
+from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 
-# Create an instance of APIRouter
-router = APIRouter()
 
 # Create an instance of FastAPI
 tiramisu = FastAPI(
-    title="Tiramisu.cat API",
-    description="Today is a fantastic day to APIfy a Tiramisu",
-    version="1.0.0",
-    openapi_url="/docs/openapi.json",
-    docs_url="/docs/documentation",
-    redoc_url="/docs/redoc",
+    title='Tiramisu.cat API',
+    description='Today is a fantastic day to APIfy a Tiramisu',
+    version='1.0.0',
+    openapi_url='/docs/openapi.json',
+    docs_url='/docs/documentation',
+    redoc_url='/docs/redoc',
 )
-
-# Include the router in the main app
-tiramisu.include_router(router, prefix="/api")
 
 
 @tiramisu.get('/stillalive', include_in_schema=False)
 async def still_alive():
     return HTMLResponse('the tiramisu is wet', 200)
+
+
+tiramisu.include_router(tiramisu_router, prefix='/api/v1')
+
+tiramisu.include_router(review_tiramisu_router, prefix='/api/v1')
+
