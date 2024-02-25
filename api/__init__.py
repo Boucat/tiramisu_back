8de57e60d0d69d52ee05ec8333ff414cd496350c
ferrannoguera <nogueravallferran@gmail.com>
2024-02-25 18:57:34 +0100

    :clown_face: Redifined the mock-up info per user

--- api/__init__.py
+++ api/__init__.py
@@ -1,8 +1,7 @@
-USERS = [
-    {
-        'sec_key': '318e010e48f6e209cbcb39254bb46547eb208029',
+USERS = {
+    '318e010e48f6e209cbcb39254bb46547eb208029': {
         'id': 'ferrannoguera',
         'name': 'Ferran Noguera',
-        'photo_url': 'https://avatars.githubusercontent.com/u/115733?v=4',
+        'photo_url': 'https://avatars.githubusercontent.com/u/29044151?v=4',
     }
-]
+}
