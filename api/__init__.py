ceabd6ea6a4e9e93dd1c7aca8abcdc633ffd7b83
ferrannoguera <nogueravallferran@gmail.com>
2024-03-09 17:32:38 +0100

    :clown_face: Redefined how mock_db is generated

--- api/__init__.py
+++ api/__init__.py
@@ -1,20 +1,30 @@
-USERS = {
-    '318e010e48f6e209cbcb39254bb46547eb208029': {
-        'id': 'ferrannoguera',
-        'name': 'Ferran Noguera',
-        'description': "Parla'm de tiramisus i et diré quantes culleretes calces. (Spoiler: No més de 6)",
+USER_MOCK_DB = {
+    'ferrannoguera': {
+        'username': 'ferrannoguera',
+        'full_name': 'Ferran Noguera',
+        'description': 'Parla de tiramisus i et diré quantes culleretes calces. (Spoiler: No més de 6)',
         'photo_url': 'https://raw.githubusercontent.com/Boucat/tiramisu_back/main/media/users/ferrannoguera.jpg',
+        'hashed_password': '$2b$12$t4WQiOBSux.0UtPxcC19qOpPwmZ1yr.d1W80OD7Qy.jBJzyYDRk1q',
+        'disabled': False,
     },
-    '93deccd1cf72c96b9f73c7c341ae07bb068b31ca': {
-        'id': 'marcklingenberg',
-        'name': 'Marc Klingenberg',
+    'marcklingenberg': {
+        'username': 'marcklingenberg',
+        'full_name': 'Marc Klingenberg',
         'description': 'Amb llicència per degustar tiramisú',
         'photo_url': 'https://raw.githubusercontent.com/Boucat/tiramisu_back/main/media/users/marcklingenberg.jpg',
+        'hashed_password': '$2b$12$pyjm6OllSwySLr4eFEPcWePYRsDtkt7Rwv6z2WzzCiiv84nJVesSa',
+        'disabled': False,
     },
-    'ea16f9559aa30d59c5edaefb35c5a57675dcb807': {
-        'id': 'albertsuarez',
-        'name': 'Albert Suarez',
+    'albertsuarez': {
+        'username': 'albertsuarez',
+        'full_name': 'Albert Suarez',
         'description': 'A punt de tastar el pitjor tiramisú georgiano de la història',
         'photo_url': 'https://raw.githubusercontent.com/Boucat/tiramisu_back/main/media/users/albertsuarez.jpg',
+        'hashed_password': '$2b$12$wL.IHscc0M7cKgBX/mGbbejCUQPKD0KDmrwbTAdi1rNxHSj8XPvGm',
+        'disabled': False,
     },
 }
+
+__all__ = [
+    'USER_MOCK_DB',
+]
