38a2e33abdc09706f3f330d3482b0ba714658202
ferrannoguera <nogueravallferran@gmail.com>
2024-03-09 15:15:48 +0100

    :pencil2: Fixed typo for ferrannoguera description

--- api/__init__.py
+++ api/__init__.py
@@ -2,7 +2,7 @@ USERS = {
     '318e010e48f6e209cbcb39254bb46547eb208029': {
         'id': 'ferrannoguera',
         'name': 'Ferran Noguera',
-        'description': "Parla'm de tiramisus i et diré quantes culleretes que calces. (Spoiler: No més de 6)",
+        'description': "Parla'm de tiramisus i et diré quantes culleretes calces. (Spoiler: No més de 6)",
         'photo_url': 'https://raw.githubusercontent.com/Boucat/tiramisu_back/main/media/users/ferrannoguera.jpg',
     },
     '93deccd1cf72c96b9f73c7c341ae07bb068b31ca': {
